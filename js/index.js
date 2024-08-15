ebf0908cad90914941fbf272e34cc19911e23703
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2024-08-15 20:40:21 +0200

    add to cart and buy function
    Added a function for buy button and add to cart button.
    Currently it logs to the console the entire object the user clicks on.
    
    Added comments to provide some explanation for what the code is doing

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 
+const productCards = document.getElementById("products")
 const API_POSTS_URL = "https://api.noroff.dev/api/v1/gamehub";
 
 async function getData() {
@@ -7,53 +8,49 @@ async function getData() {
     return json;
 }
 
+// Adding the product to cart function.
+function addToCart(product) {
+    console.log(`Product added to cart:`, product);
+}
+function purchase(product) {
+    console.log(`Purchasing`, product)
+}
 
+// Render products on the page and adding event listeners to add to cart buttons. 
 document.addEventListener("DOMContentLoaded", async () => {
-    let games = [];
-    
     try {
-        games = await getData();
+        const games = await getData();
         
+        games.forEach(game => {
+            const { id, title, onSale, discountedPrice, price, image } = game;
 
-        let data1 = "";
-       // Figure out how to not display discounted prices when product is not on discount. 
-        games.map((values) => {
+            // Display sales prices for products on sale. 
+            let displayedPrice = onSale ? price - discountedPrice: price;
 
-        let onDiscount = values.price - values.discountedPrice;   
-        let salesDisplay = values.discountedPrice;
-        
-        // Rendering each id of games in the api in a seperate card in the browser with relevant data for the page. 
-        data1 += `
-        <div class="card">
-        <p id="product-title">${values.title}</p>
-        <a href="/product/index.html"><img src=${values.image} alt="Image" id="product-image"></a>
-        <p id="category">Category: ${values.genre}</p>
-        <p id="price">Price: $${onDiscount}</p>
-        <p id="onSale">$${salesDisplay} on offer!</p>
-        <button id=${values.id} class="add-to-cart">Add to cart</button>
-        <button id=${values.id} class="buy-btn">BUY</button>         
-        </div>
-        `
-        })
-        document.getElementById("products").innerHTML = data1;
-        
+            // Render products using template literals. 
+            productCards.innerHTML += `
+            <div class="card">
+            <a href="/product/index.html"><img src=${image} alt="Image" id="product-image"></a>
+            <h2 id="product-title>${title}</h2>
+            <p id="price">Price: $${displayedPrice}</p>
+            <button id="add-to-cart-${id}" class="btn add-to-cart-btn">Add to cart</button>
+            <button id="buy-${id}" class="btn buy-btn">Buy now</button>
+            </div>
+            `
+        });
 
-        
+        // Adding event listener to add to cart buttons.
+        games.forEach(game => {
+            const addToCartBtn = document.getElementById(`add-to-cart-${game.id}`);
+            addToCartBtn.addEventListener("click", () => addToCart(game));
+        });   
+
+        // Adding event listeners to buy buttons. 
+        games.forEach(game => {
+            const buyBtn = document.getElementById(`buy-${game.id}`);
+            buyBtn.addEventListener("click", () => purchase(game));
+        }) 
     } catch (error) {
         console.log("Error:" + error);
-    }
-    
-})
-
-
-
-
- // OK so I figured out how to access the values in the GameHub API
- // Now we need to make our card. First we will create 1 card and 1 card only. 
- // After wards we will loop through the arrays creating cards for all the products. 
- // And render them on the page. 
- // Next up is the buttons to be added to each card in order for the customer
- // to purchase the products. 
-
- // And to do this we fetch the single id of the product when its added to a card? 
- // Incredible!
+    } 
+});
