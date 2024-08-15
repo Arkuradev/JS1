8095e52c2798fb9cd3bbd1ffa19a26dedc416a4c
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2024-08-15 11:15:44 +0200

    Using async await
    Changed the fetch code to use async await instead of .then

--- js/index.js
+++ js/index.js
@@ -1,85 +1,54 @@
-fetch("https://api.noroff.dev/api/v1/gamehub").then((data)=> {
-    // console.log(data)
-    return data.json();
-}).then((completeData)=>{
-    // console.log(completeData)
-    let data1 = "";
+
+
+
+async function loadGames() {
+    const response = await fetch("https://api.noroff.dev/api/v1/gamehub")
+    const games = await response.json();
+
+    return games;
+}
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    let games = [];
     
-    completeData.map((values)=>{
+    try {
+        games = await loadGames();
+        
+
+        let data1 = "";
+       // Figure out how to not display discounted prices when product is not on discount. 
+        games.map((values) => {
+
+        let onDiscount = values.price - values.discountedPrice;   
+        let salesDisplay = values.discountedPrice;
+        
+        // Rendering each id of games in the api in a seperate card in the browser with relevant data for the page. 
         data1 += `
         <div class="card">
         <p id="product-title">${values.title}</p>
-        <img src=${values.image} alt="Image" id="product-image">
-        
-        <p id="description">${values.description}</p>
+        </a><img src=${values.image} alt="Image" id="product-image">
         <p id="category">Category: ${values.genre}</p>
-        <p id="price">Price: $${values.price}</p>
-        <p id="onSale">ON SALE!</p>  
-        <button id="addCart-btn">Add to cart</button>
-        <button id="buy-btn">BUY NOW</button>         
+        <p id="price">Price: $${onDiscount}</p>
+        <p id="onSale">$${salesDisplay} on offer!</p>
+        <button id="addCart-btn" class="cart-btn">Add to cart</button>
+        <button id="buy-btn" class="">BUY</button>  
+        <a href="/product/index.html" class="description-link">Read more..</a>       
         </div>
         `
-    });
-   document.getElementById("products").innerHTML = data1;
+        })
+        document.getElementById("products").innerHTML = data1;
+        
+
+        
+    } catch (error) {
+        console.log("Error:" + error);
+    }
     
-}).catch((error)=>{
-    console.log(error)
 })
 
 
 
-
-/* 
-Research how to make it visual when a product is on sale. 
-At the same time make the price update with each product thats on sale. 
-Add a small navbar to navigate to the cart and back to products page. 
-
-
-*/
-
-
-
-
-
-/* fetchData();
-
- async function fetchData(){
-
-    try{
-        const response = await fetch("https://api.noroff.dev/api/v1/gamehub");
-
-        if(!response.ok){
-            throw new Error("Could not fetch resource");
-        }
-
-        const data = await response.json();
-        const productImage = data[0]["image"];
-        const imgElement = document.getElementById("product-image")
-        const pTitle = document.getElementById("product-title");
-        const desc = document.getElementById("description");
-        const price = document.getElementById("price");
-        const onSale = document.getElementById("on-sale");
-
-
-        imgElement.src = productImage;
-        pTitle.textContent = data[0]["title"];
-        price.textContent = data[0]["price"];
-        desc.textContent = data[0]["description"];
-        onSale.textContent = data[0]["discountedPrice"];
-
-        //Refactor this be more dynamic and functioning.
-
-        
-        for(let i = 0; i < data[i]["title"].length; i++) {
-
-            console.log(data[i]["title"])
-        }
-      
-    }catch(error){
-        console.error(error)
-    }
-
- } */
 
  // OK so I figured out how to access the values in the GameHub API
  // Now we need to make our card. First we will create 1 card and 1 card only. 
