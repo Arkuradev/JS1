0672979c343ba721e1c22ad64e407a97a0367b9d
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2024-08-21 23:24:58 +0200

    Update generateSingleItem.mjs
    Small update to include all product information on product description page.

--- js/generateSingleItem.mjs
+++ js/generateSingleItem.mjs
@@ -12,17 +12,26 @@ export function generateSingleItem(game) {
   const gameTitle = document.createElement('h2');
   gameTitle.textContent = game.title;
 
+  // Description of selected game.
+  const gameDescription = document.createElement('p');
+  gameDescription.textContent = game.description;
+
+  // Release year
+  const gameReleased = document.createElement('p');
+  gameReleased.textContent = `Released: ${game.released}`;
+
   // Game genre
   const gameGenre = document.createElement('p');
   gameGenre.textContent = `Genre: ${game.genre}`;
+
+  // Game age rating
+  const gameAgeRating = document.createElement('p');
+  gameAgeRating.textContent = `Age rating: ${game.ageRating}`;
 
   // Price of selected game.
   const gamePrice = document.createElement('p');
   gamePrice.textContent = `Price: $${game.price}`
 
-  // Description of selected game.
-  const gameDescription = document.createElement('p');
-  gameDescription.textContent = game.description;
 
   // Add to cart button with event listener to run addToCart() when clicked. 
   const gameAddToCartButton = document.createElement('button');
@@ -31,7 +40,7 @@ export function generateSingleItem(game) {
   })
   gameAddToCartButton.textContent = "Add to cart";
 
-  gameContainer.append(gameImage, gameTitle, gamePrice, gameDescription, gameGenre, gameAddToCartButton);
+  gameContainer.append(gameImage, gameTitle, gamePrice, gameDescription, gameReleased, gameGenre, gameAgeRating, gameAddToCartButton);
   return gameContainer;
 
   
