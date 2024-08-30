099e06fa04f88835f79a69358d51b19c5d894ccf
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2024-08-30 13:36:50 +0200

    Loading spinner
    Added a loading spinner to the page.

--- js/fetchData.mjs
+++ js/fetchData.mjs
@@ -1,9 +1,11 @@
 import { displayMessage } from "./common/displayMessage.mjs";
 
+
 // Add a spinner function to this. Check explanation in module 5-6 video at 1:46.
 
 export async function fetchData(url) {
   try {
+    
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error("Failed to fetch games");
